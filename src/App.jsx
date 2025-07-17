446e6e39630ea548300f30b5449b7687eab088b5
Adarsh Rai <adarshrai@Adarshs-MacBook-Air.local>
2025-07-17 20:13:25 +0530

    Updated features/fixed bugs/added new section

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   useEffect(() => {
     const stored = localStorage.getItem("theme");
     if (stored === "dark") setDarkMode(true);
